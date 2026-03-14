927d7e9ee646bca54994f7be09c14f2418723343
Szymon Nowicki <s.nowick@wp.pl>
2026-03-14 16:11:08 +0100

    updated location selecting

--- app/app.py
+++ app/app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import pandas as pd
+import joblib
 
 def main():
     st.set_page_config(page_title="Smart parking predictor", page_icon="🚘", layout="centered")
@@ -8,11 +10,50 @@ def main():
 
     location = st.selectbox("Select location", ["Centre", "Shopping mall", "Stadium", "Office area"])
     day = st.selectbox("Day of the week", ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"])
+    days = {
+        "Monday": 0,
+        "Tuesday": 1,
+        "Wednesday": 2,
+        "Thursday": 3,
+        "Friday": 4,
+        "Saturday": 5,
+        "Sunday": 6
+    }
     hour = st.slider("Hour of the day", 0, 23, 12)
     rain = st.checkbox("Is it raining?")
 
+    model = joblib.load("models/parking_model.pkl")
+
     if st.button("Predict"):
-        st.success("Prediction incoming...")
+
+        location_office_area = 0
+        location_shopping_mall = 0
+        location_stadium = 0
+
+        if location == "Office Area":
+            location_office_area = 1
+        elif location == "Shopping Mall":
+            location_shopping_mall = 1
+        elif location == "Stadium":
+            location_stadium = 1
+
+        input_data = pd.DataFrame({
+            "hour": [hour],
+            "day_of_the_week": [days[day]],
+            "temperature": [15],
+            "rain": [int(rain)],
+            "event_nearby": [0],
+            "location_office_area": [location_office_area],
+            "location_shopping_mall": [location_shopping_mall],
+            "location_stadium": [location_stadium]
+        })
+
+        prediction = model.predict(input_data)[0]
+
+        if prediction == 1:
+            st.success("Free parking spot likely available 🚗")
+        else:
+            st.error("Parking may be difficult to find ❌")
 
 
 if __name__ == "__main__":
